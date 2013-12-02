cad647afdf7fe222fdfe18dedaa8a66ef2194875
sbrissen <sbrissen@hotmail.com>
2013-12-02 08:17:58 -0500

    i925: update build desc/fingerprint, access vzw apps from playstore
    credit to Matt Mower to vzw apps access
    
    Change-Id: Ia5a527c71f1ce53ae66d50c6f1661cc2a1b195ed

--- cm.mk
+++ cm.mk
@@ -17,5 +17,8 @@ PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SCH-I925
 PRODUCT_MANUFACTURER := samsung
 
+PRODUCT_GMS_CLIENTID_BASE := android-verizon
+
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=p4noteltevzw TARGET_DEVICE=p4noteltevzw BUILD_FINGERPRINT="Verizon/p4noteltevzw/p4noteltevzw:4.1.2/JZO54K/I925VRALL3:user/release-keys" PRIVATE_BUILD_DESC="p4noteltevzw-user 4.1.2 JZO54K I925VRALL3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=p4noteltevzw TARGET_DEVICE=p4noteltevzw BUILD_FINGERPRINT="Verizon/p4noteltevzw/p4noteltevzw:4.1.2/JZO54K/I925VRAME2:user/release-keys" PRIVATE_BUILD_DESC="p4noteltevzw-user 4.1.2 JZO54K I925VRAME2 release-keys"
+
