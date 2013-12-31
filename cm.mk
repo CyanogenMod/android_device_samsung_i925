9cda4cca5f23706cbe83f5e732711a6b084f89c5
sbrissen <sbrissen@hotmail.com>
2013-12-31 07:42:04 -0500

    i925: add telephony support
    Change-Id: I941dd394b2e6bfaad158ebb0d39a98f72833b45c

--- cm.mk
+++ cm.mk
@@ -5,7 +5,7 @@ $(call inherit-product, vendor/cm/config/cdma.mk)
 PRODUCT_RELEASE_NAME := i925
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i925/full_i925.mk)
